eb7279f6e4568415d9c9be1a00fe2bdedf8e81f4
Vishal Sharma <vsh1819@gmail.com>
2018-07-04 18:15:22 +0000

    automatically backed up by learn

---
+++ lib/author.rb
@@ -0,0 +1,25 @@
+class Author 
+  attr_accessor :name, :songs
+  
+  @@total = 0 
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def add_song(song)
+    song.artist = self
+    @songs << song
+    @@total += 1
+  end
+  
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    add_song(song)
+  end
+  
+  def self.song_count
+    @@total
+  end
+end
